267cf9a1ce54b0257d4b769e19f3112e509e0faa
Kevin Tran <kevin2005tran@gmail.com>
2017-04-04 20:32:07 -0700

    added 9 products to database

--- databaseFiles/MCMKevin.js
+++ databaseFiles/MCMKevin.js
@@ -31,4 +31,59 @@
   "storyCategory" : "midCenturyModern",
   "roomCategory" : ["kitchen"],
   "productCategory" : ["diningware"]
+}, {
+  "name" : "Bread Maker",
+  "productID" : "",
+  "price" : 83.97,
+  "description" : "Large LCD readout, measures 11 1/8-inch (Width) by 12 13/16-inch (Length) by 14 9/16-inch (Height), 15-pound",
+  "specifications" : ["Size/Weight W 21\" / D 18\" / H 15\" / 20 lb."],
+  "reviews" : [],
+  "imageURL" : "https://st.hzcdn.com/simgs/5e61530407d99141_4-4738/modern-small-kitchen-appliances.jpg",
+  "storyCategory" : "midCenturyModern",
+  "roomCategory" : ["kitchen"],
+  "productCategory" : ["smallAppliance"]
+}, {
+  "name" : "Grid-Tufted Upholstered Tapered Leg Bed",
+  "productID" : "",
+  "price" : 299.99,
+  "description" : "Tuft love. Upholstered in a steel gray linen weave, our Grid-Tufted Tapered Leg Bed puts a retro spin on a classic silhouette.",
+  "specifications" : ["Solid wood legs in a Walnut finish.", "Solid pine support slats included."],
+  "reviews" : [],
+  "imageURL" : "http://rk.weimgs.com/weimgs/rk/images/wcm/products/201703/0002/grid-tufted-upholstered-tapered-leg-bed-in-heathered-cross-c.jpg",
+  "storyCategory" : "midCenturyModern",
+  "roomCategory" : ["bedroom"],
+  "productCategory" : ["furniture"]
+}, {
+  "name" : "Area Bedding Kline Design",
+  "productID" : "",
+  "price" : 99.90,
+  "description" : "Geometric patterned matelasse coverlet in ivory, grey and white made of 100% cotton.",
+  "specifications" : ["Twin Coverlet: 69 x 90 Inch", "Full/Queen Coverlet: 90 x 90 Inch"],
+  "reviews" : [],
+  "imageURL" : "http://modernplanet.com/media/catalog/product/cache/2/image/1800x/040ec09b1e35df139433887a97daa66f/k/l/kline_2.jpg",
+  "storyCategory" : "midCenturyModern",
+  "roomCategory" : ["bedroom"],
+  "productCategory" : ["textile"]
+}, {
+  "name" : "Springfield Linen Shade Ceiling Light",
+  "productID" : "",
+  "price" : 79.45,
+  "description" : "Create a clean look on your ceiling with a linen drum shade ceiling light.",
+  "specifications" : ["(7.25\"Hx15.75\"W)", "3x40 watts (medium base socket)"],
+  "reviews" : [],
+  "imageURL" : "https://s-media-cache-ak0.pinimg.com/736x/86/25/53/8625532470cef0603cb9c7d0b314c7c8.jpg",
+  "storyCategory" : "midCenturyModern",
+  "roomCategory" : ["bedroom"],
+  "productCategory" : ["lighting"]
+}, {
+  "name" : "Penelope Nightstand – Acorn",
+  "productID" : "",
+  "price" : 49.14,
+  "description" : "Well-rounded. Inspired by mid-century design, our Penelope Nightstand stands on slim, tapered legs.",
+  "specifications" : ["Solid marble table top", "Made in Vietnam"],
+  "reviews" : [],
+  "imageURL" : "http://ab.weimgs.com/weimgs/ab/images/wcm/products/201639/0085/penelope-nightstand-acorn-c.jpg",
+  "storyCategory" : "midCenturyModern",
+  "roomCategory" : ["bedroom"],
+  "productCategory" : ["accessory"]
 }]
