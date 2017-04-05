3425e837f81ba52683ba4801f5ea25e6fce95f63
Kevin Tran <kevin2005tran@gmail.com>
2017-04-04 17:36:40 -0700

    first commit

---
+++ Veedu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Veedu
+//
+//  Created by Kevin Tran on 4/4/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
