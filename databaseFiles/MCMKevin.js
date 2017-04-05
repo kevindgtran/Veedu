842021d0ba2e5c42191022afecd4e36d3a26d2cb
Kevin Tran <kevin2005tran@gmail.com>
2017-04-04 19:50:45 -0700

    added sktch file and started building out database products

---
+++ databaseFiles/MCMKevin.js
@@ -0,0 +1,34 @@
+"allProducts": [{
+  "name" : "Mid-Century Expandable Dining Table",
+  "productID" : "",
+  "price" : 299.99,
+  "description" : "Room to grow. Bring the beauty of beveled edges, shapely solid wood legs and mid-century design to the dining room with this expandable dining table.",
+  "specifications" : ["Overall product dimensions (without leaf): 39\"w x 32\"d x 30\"h.", "Clearance under table: 29\""],
+  "reviews" : [],
+  "imageURL" : "http://rk.weimgs.com/weimgs/rk/images/wcm/products/201711/0060/mid-century-expandable-dining-table-o.jpg",
+  "storyCategory" : "midCenturyModern",
+  "roomCategory" : ["kitchen"],
+  "productCategory" : ["furniture"]
+}, {
+  "name" : "Handle Me Cast Iron Cookware",
+  "productID" : "",
+  "price" : 89.95,
+  "description" : "Designed by Simen Aarseth, Christoffer Angell and Øyvind Wyller, designer and owner of Angell Wyller Aarseth, an Oslo, Norway based studio, the contemporary cast iron cookware offer unique design for your kitchenware.",
+  "specifications" : ["Cast Iron"],
+  "reviews" : [],
+  "imageURL" : "http://www.newyorkmarkt.com/pictures/Contemporary-Cast-Iron-Cookware-for-Kitchenware-Design-Handle-Me-by-Angell-Wyller-Aarseth-1.jpg",
+  "storyCategory" : "midCenturyModern",
+  "roomCategory" : ["kitchen"],
+  "productCategory" : ["cookware"]
+}, {
+  "name" : "4-Piece Square Place Setting",
+  "productID" : "",
+  "price" : 49.85,
+  "description" : "Elevate your everyday dining experience with Noritake's contemporary NoN Swirl Square Dinnerware Collection, part of the BoB and WoW pattern.",
+  "specifications" : ["One 11\" dinner plate", "One 8.25\" salad plate", "One 6\" cereal bowl", "One 12 oz. mug"],
+  "reviews" : [],
+  "imageURL" : "https://s7d9.scene7.com/is/image/BedBathandBeyond/79250246191090p",
+  "storyCategory" : "midCenturyModern",
+  "roomCategory" : ["kitchen"],
+  "productCategory" : ["diningware"]
+}]
