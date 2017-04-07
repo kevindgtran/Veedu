6878ace630976a476b0b28c24303a628b0034122
Kevin Tran <kevin2005tran@gmail.com>
2017-04-07 14:21:37 -0700

    added 7 product reviews

--- databaseFiles/MCMKevin.js
+++ databaseFiles/MCMKevin.js
@@ -1,158 +1,157 @@
-"allProducts": [{
-  "name" : "Mid-Century Expandable Dining Table",
-  "productID" : "",
-  "price" : 299.99,
-  "description" : "Room to grow. Bring the beauty of beveled edges, shapely solid wood legs and mid-century design to the dining room with this expandable dining table.",
-  "specifications" : ["Overall product dimensions (without leaf): 39\"w x 32\"d x 30\"h.", "Clearance under table: 29\""],
-  "reviews" : [],
-  "imageURL" : "http://rk.weimgs.com/weimgs/rk/images/wcm/products/201711/0060/mid-century-expandable-dining-table-o.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["kitchen"],
-  "productCategory" : ["furniture"]
-}, {
-  "name" : "Handle Me Cast Iron Cookware",
-  "productID" : "",
-  "price" : 89.95,
-  "description" : "Designed by Simen Aarseth, Christoffer Angell and Øyvind Wyller, designer and owner of Angell Wyller Aarseth, an Oslo, Norway based studio, the contemporary cast iron cookware offer unique design for your kitchenware.",
-  "specifications" : ["Cast Iron"],
-  "reviews" : [],
-  "imageURL" : "http://www.newyorkmarkt.com/pictures/Contemporary-Cast-Iron-Cookware-for-Kitchenware-Design-Handle-Me-by-Angell-Wyller-Aarseth-1.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["kitchen"],
-  "productCategory" : ["cookware"]
-}, {
-  "name" : "4-Piece Square Place Setting",
-  "productID" : "",
-  "price" : 49.85,
-  "description" : "Elevate your everyday dining experience with Noritake's contemporary NoN Swirl Square Dinnerware Collection, part of the BoB and WoW pattern.",
-  "specifications" : ["One 11\" dinner plate", "One 8.25\" salad plate", "One 6\" cereal bowl", "One 12 oz. mug"],
-  "reviews" : [],
-  "imageURL" : "https://s7d9.scene7.com/is/image/BedBathandBeyond/79250246191090p",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["kitchen"],
-  "productCategory" : ["diningware"]
-}, {
-  "name" : "Bread Maker",
-  "productID" : "",
-  "price" : 83.97,
-  "description" : "Large LCD readout, measures 11 1/8-inch (Width) by 12 13/16-inch (Length) by 14 9/16-inch (Height), 15-pound",
-  "specifications" : ["Size/Weight W 21\" / D 18\" / H 15\" / 20 lb."],
-  "reviews" : [],
-  "imageURL" : "https://st.hzcdn.com/simgs/5e61530407d99141_4-4738/modern-small-kitchen-appliances.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["kitchen"],
-  "productCategory" : ["smallAppliance"]
-}, {
-  "name" : "Grid-Tufted Upholstered Tapered Leg Bed",
-  "productID" : "",
-  "price" : 299.99,
-  "description" : "Tuft love. Upholstered in a steel gray linen weave, our Grid-Tufted Tapered Leg Bed puts a retro spin on a classic silhouette.",
-  "specifications" : ["Solid wood legs in a Walnut finish.", "Solid pine support slats included."],
-  "reviews" : [],
-  "imageURL" : "http://rk.weimgs.com/weimgs/rk/images/wcm/products/201703/0002/grid-tufted-upholstered-tapered-leg-bed-in-heathered-cross-c.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["bedRoom"],
-  "productCategory" : ["furniture"]
-}, {
-  "name" : "Area Bedding Kline Design",
-  "productID" : "",
-  "price" : 99.90,
-  "description" : "Geometric patterned matelasse coverlet in ivory, grey and white made of 100% cotton.",
-  "specifications" : ["Twin Coverlet: 69 x 90 Inch", "Full/Queen Coverlet: 90 x 90 Inch"],
-  "reviews" : [],
-  "imageURL" : "http://modernplanet.com/media/catalog/product/cache/2/image/1800x/040ec09b1e35df139433887a97daa66f/k/l/kline_2.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["bedRoom"],
-  "productCategory" : ["textile"]
-}, {
-  "name" : "Springfield Linen Shade Ceiling Light",
-  "productID" : "",
-  "price" : 79.45,
-  "description" : "Create a clean look on your ceiling with a linen drum shade ceiling light.",
-  "specifications" : ["(7.25\"Hx15.75\"W)", "3x40 watts (medium base socket)"],
-  "reviews" : [],
-  "imageURL" : "https://s-media-cache-ak0.pinimg.com/736x/86/25/53/8625532470cef0603cb9c7d0b314c7c8.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["bedRoom"],
-  "productCategory" : ["lighting"]
-}, {
-  "name" : "Penelope Nightstand – Acorn",
-  "productID" : "",
-  "price" : 49.14,
-  "description" : "Well-rounded. Inspired by mid-century design, our Penelope Nightstand stands on slim, tapered legs.",
-  "specifications" : ["Solid marble table top", "Made in Vietnam"],
-  "reviews" : [],
-  "imageURL" : "http://ab.weimgs.com/weimgs/ab/images/wcm/products/201639/0085/penelope-nightstand-acorn-c.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["bedRoom"],
-  "productCategory" : ["accessory"]
-}]
-
-
-"allProducts2": [{
-  "name" : "Modern Urban Sofa",
-  "productID" : "",
-  "price" : 479,
-  "description" : "The Urban's simple silhouette and sink-right-in comfort make it a classic choice for the family room.",
-  "specifications" : ["Wood frame", "84.5\"w x 39\"d x 31\"h"],
-  "reviews" : [],
-  "imageURL" : "https://s-media-cache-ak0.pinimg.com/564x/c7/93/6a/c7936a40e4959398a2e2a5f861c4dfd2.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["livingRoom"],
-  "productCategory" : ["furniture"]
-}, {
-  "name" : "Modern Circles Area Rug",
-  "productID" : "",
-  "price" : 125.24,
-  "description" : "This beautiful rug is unique, stylish and ready to accent your decor with authentic elegance.",
-  "specifications" : ["Made of Polypropylene", "Machine made in Turkey"],
-  "reviews" : [],
-  "imageURL" : "http://www.homedepot.com/catalog/productImages/1000/68/68ed6c08-3a19-4600-8c7d-ea8fb972724f_1000.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["livingRoom"],
-  "productCategory" : ["textile"]
-}, {
-  "name" : "Discus Ceiling Light",
-  "productID" : "",
-  "price" : 115.40,
-  "description" : "The Discus lighting collection ceiling fixture features a clean modern living flair.",
-  "specifications" : ["1-light semi-flushmount ceiling fixture in nickel finish", "Black shade"],
-  "reviews" : [],
-  "imageURL" : "http://www.homedepot.com/catalog/productImages/1000/4b/4be1e63e-736e-4ffa-b096-e1c8ae425b8d_1000.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["livingRoom"],
-  "productCategory" : ["lighting"]
-}, {
-  "name" : "Reeded Sea Glass Wall Mirror",
-  "productID" : "",
-  "price" : 45.79,
-  "description" : "This frameless aqua/blue mosaic tile composition resembles aged sea glass.",
-  "specifications" : ["Beautiful aqua/blue mosaic tile design", "Hand crafted"],
-  "reviews" : [],
-  "imageURL" : "http://www.homedepot.com/catalog/productImages/400/99/99460b0a-0dbc-4925-a3af-d9b537fe8903_400.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["livingRoom"],
-  "productCategory" : ["accessory"]
-}, {
-  "name" : "Jacquard Egyptian Cotton 6-Piece Towel Set, Blue Stone",
-  "productID" : "",
-  "price" : 28.04,
-  "description" : "Enjoy comfort and style with these beautiful double ply 100-percent Egyptian cotton.",
-  "specifications" : ["100-percent Egyptian cotton", "Machine washable"],
-  "reviews" : [],
-  "imageURL" : "https://st.hzcdn.com/fimgs/5dd14af60804be88_0428-w233-h233-b1-p10--modern-bath-towels.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["bathRoom"],
-  "productCategory" : ["textile"]
-}, {
-  "name" : "Modern Bathroom Glass & Metal Set",
-  "productID" : "",
-  "price" : 28.85,
-  "description" : "Sleek & chic. In beautiful glass and stainless steel with clean, contemporary lines",
-  "specifications" : ["glass set"],
-  "reviews" : [],
-  "imageURL" : "http://teaone.net/uploads/fotos/modern-bathroom-accessories-sets_513_750_595.jpg",
-  "storyCategory" : "midCenturyModern",
-  "roomCategory" : ["bathRoom"],
-  "productCategory" : ["accessory"]
-}]
+{
+	"allProducts": [{
+		"name": "Mid-Century Expandable Dining Table",
+		"productID": "kpro001",
+		"price": 299.99,
+		"description": "Room to grow. Bring the beauty of beveled edges, shapely solid wood legs and mid-century design to the dining room with this expandable dining table.",
+		"specifications": ["Overall product dimensions (without leaf): 39\"w x 32\"d x 30\"h.", "Clearance under table: 29\""],
+		"reviews": [],
+		"imageURL": "http://rk.weimgs.com/weimgs/rk/images/wcm/products/201711/0060/mid-century-expandable-dining-table-o.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["kitchen"],
+		"productCategory": ["furniture"]
+	}, {
+		"name": "Handle Me Cast Iron Cookware",
+		"productID": "kpro008",
+		"price": 89.95,
+		"description": "Designed by Simen Aarseth, Christoffer Angell and Øyvind Wyller, designer and owner of Angell Wyller Aarseth, an Oslo, Norway based studio, the contemporary cast iron cookware offer unique design for your kitchenware.",
+		"specifications": ["Cast Iron"],
+		"reviews": [],
+		"imageURL": "http://www.newyorkmarkt.com/pictures/Contemporary-Cast-Iron-Cookware-for-Kitchenware-Design-Handle-Me-by-Angell-Wyller-Aarseth-1.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["kitchen"],
+		"productCategory": ["cookware"]
+	}, {
+		"name": "4-Piece Square Place Setting",
+		"productID": "kpro002",
+		"price": 49.85,
+		"description": "Elevate your everyday dining experience with Noritake's contemporary NoN Swirl Square Dinnerware Collection, part of the BoB and WoW pattern.",
+		"specifications": ["One 11\" dinner plate", "One 8.25\" salad plate", "One 6\" cereal bowl", "One 12 oz. mug"],
+		"reviews": [],
+		"imageURL": "https://s7d9.scene7.com/is/image/BedBathandBeyond/79250246191090p",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["kitchen"],
+		"productCategory": ["diningware"]
+	}, {
+		"name": "Bread Maker",
+		"productID": "kpro009",
+		"price": 83.97,
+		"description": "Large LCD readout, measures 11 1/8-inch (Width) by 12 13/16-inch (Length) by 14 9/16-inch (Height), 15-pound",
+		"specifications": ["Size/Weight W 21\" / D 18\" / H 15\" / 20 lb."],
+		"reviews": [],
+		"imageURL": "https://st.hzcdn.com/simgs/5e61530407d99141_4-4738/modern-small-kitchen-appliances.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["kitchen"],
+		"productCategory": ["smallAppliance"]
+	}, {
+		"name": "Grid-Tufted Upholstered Tapered Leg Bed",
+		"productID": "kpro003",
+		"price": 299.99,
+		"description": "Tuft love. Upholstered in a steel gray linen weave, our Grid-Tufted Tapered Leg Bed puts a retro spin on a classic silhouette.",
+		"specifications": ["Solid wood legs in a Walnut finish.", "Solid pine support slats included."],
+		"reviews": [],
+		"imageURL": "http://rk.weimgs.com/weimgs/rk/images/wcm/products/201703/0002/grid-tufted-upholstered-tapered-leg-bed-in-heathered-cross-c.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["bedRoom"],
+		"productCategory": ["furniture"]
+	}, {
+		"name": "Area Bedding Kline Design",
+		"productID": "kpro010",
+		"price": 99.90,
+		"description": "Geometric patterned matelasse coverlet in ivory, grey and white made of 100% cotton.",
+		"specifications": ["Twin Coverlet: 69 x 90 Inch", "Full/Queen Coverlet: 90 x 90 Inch"],
+		"reviews": [],
+		"imageURL": "http://modernplanet.com/media/catalog/product/cache/2/image/1800x/040ec09b1e35df139433887a97daa66f/k/l/kline_2.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["bedRoom"],
+		"productCategory": ["textile"]
+	}, {
+		"name": "Springfield Linen Shade Ceiling Light",
+		"productID": "kpro004",
+		"price": 79.45,
+		"description": "Create a clean look on your ceiling with a linen drum shade ceiling light.",
+		"specifications": ["(7.25\"Hx15.75\"W)", "3x40 watts (medium base socket)"],
+		"reviews": [],
+		"imageURL": "https://s-media-cache-ak0.pinimg.com/736x/86/25/53/8625532470cef0603cb9c7d0b314c7c8.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["bedRoom"],
+		"productCategory": ["lighting"]
+	}, {
+		"name": "Penelope Nightstand – Acorn",
+		"productID": "kpro005",
+		"price": 49.14,
+		"description": "Well-rounded. Inspired by mid-century design, our Penelope Nightstand stands on slim, tapered legs.",
+		"specifications": ["Solid marble table top", "Made in Vietnam"],
+		"reviews": [],
+		"imageURL": "http://ab.weimgs.com/weimgs/ab/images/wcm/products/201639/0085/penelope-nightstand-acorn-c.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["bedRoom"],
+		"productCategory": ["accessory"]
+	}, {
+		"name": "Modern Urban Sofa",
+		"productID": "kpro011",
+		"price": 479,
+		"description": "The Urban's simple silhouette and sink-right-in comfort make it a classic choice for the family room.",
+		"specifications": ["Wood frame", "84.5\"w x 39\"d x 31\"h"],
+		"reviews": [],
+		"imageURL": "https://s-media-cache-ak0.pinimg.com/564x/c7/93/6a/c7936a40e4959398a2e2a5f861c4dfd2.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["livingRoom"],
+		"productCategory": ["furniture"]
+	}, {
+		"name": "Modern Circles Area Rug",
+		"productID": "kpro006",
+		"price": 125.24,
+		"description": "This beautiful rug is unique, stylish and ready to accent your decor with authentic elegance.",
+		"specifications": ["Made of Polypropylene", "Machine made in Turkey"],
+		"reviews": [],
+		"imageURL": "http://www.homedepot.com/catalog/productImages/1000/68/68ed6c08-3a19-4600-8c7d-ea8fb972724f_1000.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["livingRoom"],
+		"productCategory": ["textile"]
+	}, {
+		"name": "Discus Ceiling Light",
+		"productID": "kpro012",
+		"price": 115.40,
+		"description": "The Discus lighting collection ceiling fixture features a clean modern living flair.",
+		"specifications": ["1-light semi-flushmount ceiling fixture in nickel finish", "Black shade"],
+		"reviews": [],
+		"imageURL": "http://www.homedepot.com/catalog/productImages/1000/4b/4be1e63e-736e-4ffa-b096-e1c8ae425b8d_1000.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["livingRoom"],
+		"productCategory": ["lighting"]
+	}, {
+		"name": "Reeded Sea Glass Wall Mirror",
+		"productID": "kpro007",
+		"price": 45.79,
+		"description": "This frameless aqua/blue mosaic tile composition resembles aged sea glass.",
+		"specifications": ["Beautiful aqua/blue mosaic tile design", "Hand crafted"],
+		"reviews": [],
+		"imageURL": "http://www.homedepot.com/catalog/productImages/400/99/99460b0a-0dbc-4925-a3af-d9b537fe8903_400.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["livingRoom"],
+		"productCategory": ["accessory"]
+	}, {
+		"name": "Jacquard Egyptian Cotton 6-Piece Towel Set, Blue Stone",
+		"productID": "kpro013",
+		"price": 28.04,
+		"description": "Enjoy comfort and style with these beautiful double ply 100-percent Egyptian cotton.",
+		"specifications": ["100-percent Egyptian cotton", "Machine washable"],
+		"reviews": [],
+		"imageURL": "https://st.hzcdn.com/fimgs/5dd14af60804be88_0428-w233-h233-b1-p10--modern-bath-towels.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["bathRoom"],
+		"productCategory": ["textile"]
+	}, {
+		"name": "Modern Bathroom Glass & Metal Set",
+		"productID": "kpro014",
+		"price": 28.85,
+		"description": "Sleek & chic. In beautiful glass and stainless steel with clean, contemporary lines",
+		"specifications": ["glass set"],
+		"reviews": [],
+		"imageURL": "http://teaone.net/uploads/fotos/modern-bathroom-accessories-sets_513_750_595.jpg",
+		"storyCategory": "midCenturyModern",
+		"roomCategory": ["bathRoom"],
+		"productCategory": ["accessory"]
+	}]
+}
