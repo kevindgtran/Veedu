ce4343226b28d62ab37cc9eadb786ef8d05114e2
Joy Umali <joyumali@gmail.coml>
2017-04-07 17:02:26 -0700

    prep for end of day pull from KevinBranch

---
+++ Veedu/BrowseCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  BrowseCollectionViewCell.swift
+//  Veedu
+//
+//  Created by Joy Umali on 4/7/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+import UIKit
+
+class BrowseCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+//        productImage.image = nil
+    }
+    
+    
+}
--- Veedu/BrowseVC.swift
+++ Veedu/BrowseVC.swift
@@ -33,3 +33,5 @@ class BrowseVC: UIViewController {
     */
 
 }
+
+extension 
